d61b124166bd7f7c3f65f1cff0efa9710fd7ec00
charkaoui007 <charkaoui1.anass@gmail.com>
2024-01-21 11:28:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# client-voiture-Microservices-Docker
+
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+  - [1. Clone the Repository](#1-clone-the-repository)
+  - [2. Build and Run the Services](#2-build-and-run-the-services)
+  - [3. Access the Application](#3-access-the-application)
+- [Development](#development)
+- [Cleanup](#cleanup)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- [Docker](https://www.docker.com/)
+- [Docker Compose](https://docs.docker.com/compose/)
+
+## Getting Started
+
+Follow these steps to get the microservices app up and running.
+
+### 1. Clone the Repository
+### 1. Run docker-compose up --build
+### 1. Stop with docker-compose down
+
+
+
---
+++ Voiture/src/main/java/ma/emsi/Voiture/model/Voiture.java
@@ -0,0 +1,63 @@
+package ma.emsi.Voiture.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+
+public class Voiture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String marque;
+    private String matricule;
+    private String model;
+    @Transient @ManyToOne
+    private Client client;
+	public Voiture(Long id, String marque, String matricule, String model, Client client) {
+		super();
+		this.id = id;
+		this.marque = marque;
+		this.matricule = matricule;
+		this.model = model;
+		this.client = client;
+	}
+	public Voiture() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+    
+    
+}
